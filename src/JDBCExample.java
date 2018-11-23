ae0ba8f7f87e08c4bee3b7d756d41a723078a036
Richa Wadaskar <richa.wadaskar@gmail.com>
2018-11-23 15:56:04 -0800

    Refactored accounts and customers table

--- src/JDBCExample.java
+++ src/JDBCExample.java
@@ -11,21 +11,46 @@ public class JDBCExample {
     static final String PASSWORD = System.getenv("PASSWORD");
 
     public static void main(String[] args) {
-        createCustomersTable();
-        //createAccountsTable();
+        createTables();
     }
 
-    public static void createCustomersTable() {
+    public static void createCustomersTable(Statement stmt) throws SQLException {
+        String sql = "INSERT INTO Customers (taxId, name, address, pin) VALUES (001, 'Denise', 'SB', '1121')";
+        int result = stmt.executeUpdate(sql);
+        System.out.println("num rows edited: " + result);
+    }
+
+    public static void createAccountsTable(Statement stmt) throws SQLException{
+        //String s1 = "DROP TABLE Accounts";
+        //String sql = "CREATE TABLE IF NOT EXISTS 'Accounts' (accountId int, primaryOwner varchar(255), bankBranchName varchar(255))";
+        //String sql2 = "INSERT INTO Accounts (accountId, primaryOwner, bankBranchName) VALUES (111, 'Richard', 'bofa')";
+        String sql3 = "SELECT accountId, primaryOwner FROM Accounts";
+        //ResultSet result = stmt.executeQuery(sql);
+        //int result2 = stmt.executeUpdate(s1);
+        //System.out.println("Insert items: " + result2);
+
+        ResultSet result3 = stmt.executeQuery(sql3);
+        while(result3.next()) {
+            String aid = result3.getString("accountId");
+            String owner = result3.getString("primaryOwner");
+            System.out.print("accountId: " + aid);
+            System.out.println(", owner: " + owner);
+        }
+        result3.close();
+    }
+
+    public static void createTables() {
         Connection conn = null;
         Statement stmt = null;
-        try{
+        try {
             Class.forName(JDBC_DRIVER);
             conn = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
             stmt = conn.createStatement();
 
-            String sql = "CREATE TABLE Customers (taxId int, name varchar(255), address varchar(255), pin char(4), PRIMARY KEY(taxId))";
-            ResultSet result = stmt.executeQuery(sql);
-        }catch(SQLException se){
+            createCustomersTable(stmt);
+            createAccountsTable(stmt);
+
+        } catch(SQLException se){
             se.printStackTrace();
         }catch(Exception e){
             e.printStackTrace();
@@ -43,51 +68,4 @@ public class JDBCExample {
             }
         }
     }
-
-    public static void createAccountsTable(){
-        Connection conn = null;
-        Statement stmt = null;
-        try{
-            Class.forName(JDBC_DRIVER);
-            conn = DriverManager.getConnection(DB_URL, USERNAME, PASSWORD);
-            stmt = conn.createStatement();
-
-            //String s1 = "DROP TABLE Accounts";
-            //String sql = "CREATE TABLE IF NOT EXISTS 'Accounts' (accountId int, primaryOwner varchar(255), bankBranchName varchar(255))";
-            //String sql2 = "INSERT INTO Accounts (accountId, primaryOwner, bankBranchName) VALUES (111, 'Richard', 'bofa')";
-            String sql3 = "SELECT accountId, primaryOwner FROM Accounts";
-            //ResultSet result = stmt.executeQuery(sql);
-            //int result2 = stmt.executeUpdate(s1);
-            //System.out.println("Insert items: " + result2);
-
-            ResultSet result3 = stmt.executeQuery(sql3);
-            while(result3.next()) {
-                String aid = result3.getString("accountId");
-                String owner = result3.getString("primaryOwner");
-                System.out.print("accountId: " + aid);
-                System.out.println(", owner: " + owner);
-            }
-            result3.close();
-        }catch(SQLException se){
-            //Handle errors for JDBC
-            se.printStackTrace();
-        }catch(Exception e){
-            //Handle errors for Class.forName
-            e.printStackTrace();
-        }finally{
-            //finally block used to close resources
-            try{
-                if(stmt!=null)
-                    conn.close();
-            }catch(SQLException se){
-            }// do nothing
-            try{
-                if(conn!=null)
-                    conn.close();
-            }catch(SQLException se){
-                se.printStackTrace();
-            }//end finally try
-        }//end try
-        System.out.println("Goodbye!");
-    }//end main
-}//end JDBCExample
+}
